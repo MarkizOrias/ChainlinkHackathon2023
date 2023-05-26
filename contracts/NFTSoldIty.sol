17d8b6482274a0f2d86cc8f67672251265a5f89f
MarkizOrias <georgi.bargan@gmail.com>
2023-05-27 01:06:13 +0200

    contract structure

--- contracts/NFTSoldIty.sol
+++ contracts/NFTSoldIty.sol
@@ -2,7 +2,32 @@
 pragma solidity 0.8.18;
 
 import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFTSoldIty is ERC721A {
     constructor() ERC721A("NFTSoldIty", "NFTSI") {}
+
+    /**
+     * @dev Functions list private, public and external
+     */
+
+    function mintNFT() external onlyOwner {}
+
+    function tokenURI() public {}
+
+    function approve() public {}
+
+    function withdrawMoney() private onlyOwner {}
+
+    function renewAuction() external {}
+
+    modifier biddingStateCheck() {}
+
+    function getHighestBidder() external {}
+
+    function getHighestBid() external {}
+
+    function getTime() external {}
+
+    function getPendingReturns() external {}
 }
