dc40e50111ce4cbf202d56786b24619beb5d81cd
MarkizOrias <georgi.bargan@gmail.com>
2023-05-27 01:00:33 +0200

    contract type

---
+++ contracts/NFTSoldIty.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import "erc721a/contracts/ERC721A.sol";
+
+contract NFTSoldIty is ERC721A {
+    constructor() ERC721A("NFTSoldIty", "NFTSI") {}
+}
