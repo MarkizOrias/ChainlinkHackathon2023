e8be018ca1bcf76ef36f01db9070875865a76e6c
MarkizOrias <georgi.bargan@gmail.com>
2023-05-27 01:19:32 +0200

    Minting done

--- contracts/NFTSoldIty.sol
+++ contracts/NFTSoldIty.sol
@@ -8,13 +8,18 @@ contract NFTSoldIty is ERC721A, Ownable {
     // Structs
 
     struct Auction {
+        uint256 s_tokenIdToBid;
         string s_tokenIdToTokenURI;
+        uint256 s_tokenIdToAuctionStart;
+        uint256 s_tokenIdToAuctionDuration;
     }
     // Mappings
     mapping(uint256 => Auction) private auctions;
 
     // Events
     event NFT_Minted(address indexed minter, uint256 indexed tokenId);
+    event NFT_SetTokenURI(string uri, uint256 indexed tokenId);
+    event NFT_AuctionTimeUpdated(uint256 indexed time, uint256 indexed tokenId);
 
     constructor() ERC721A("NFTSoldIty", "NFTSI") {}
 
@@ -22,7 +27,23 @@ contract NFTSoldIty is ERC721A, Ownable {
     function mintNFT(string memory externalTokenURI, uint256 auctionDuration)
         external
         onlyOwner
-    {}
+    {
+        if (auctionDuration < 10) revert Abstract__AuctionDurationTooShort();
+
+        uint256 newTokenId = totalSupply();
+        Auction storage auction = auctions[newTokenId];
+
+        _mint(msg.sender, 1);
+
+        auction.s_tokenIdToBid = startPrice;
+        auction.s_tokenIdToTokenURI = externalTokenURI;
+        auction.s_tokenIdToAuctionStart = block.timestamp;
+        auction.s_tokenIdToAuctionDuration = auctionDuration;
+
+        emit NFT_Minted(msg.sender, newTokenId);
+        emit NFT_SetTokenURI(auction.s_tokenIdToTokenURI, newTokenId);
+        emit NFT_AuctionTimeUpdated(auctionDuration, newTokenId);
+    }
 
     function tokenURI(uint256 tokenId)
         override
