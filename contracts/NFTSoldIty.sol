a9c9020e64ca3fd9a36c86d3ec39dd9c1e4901fe
MarkizOrias <georgi.bargan@gmail.com>
2023-05-27 01:15:07 +0200

    mint NFT

--- contracts/NFTSoldIty.sol
+++ contracts/NFTSoldIty.sol
@@ -4,14 +4,36 @@ pragma solidity 0.8.18;
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTSoldIty is ERC721A {
+contract NFTSoldIty is ERC721A, Ownable {
+    // Structs
+
+    struct Auction {
+        string s_tokenIdToTokenURI;
+    }
+    // Mappings
+    mapping(uint256 => Auction) private auctions;
+
+    // Events
+    event NFT_Minted(address indexed minter, uint256 indexed tokenId);
+
     constructor() ERC721A("NFTSoldIty", "NFTSI") {}
 
     /**
      * @dev Functions list private, public and external
      */
 
-    function mintNFT() external onlyOwner {}
+    function mintNFT(string memory externalTokenURI, uint256 auctionDuration)
+        external
+        onlyOwner
+    {
+        uint256 newTokenId = totalSupply();
+        Auction storage auction = auctions[newTokenId];
+
+        auction.s_tokenIdToTokenURI = externalTokenURI;
+        auction.s_tokenIdToAuctionDuration = auctionDuration;
+
+        emit NFT_Minted(msg.sender, newTokenId);
+    }
 
     function tokenURI() public {}
 
